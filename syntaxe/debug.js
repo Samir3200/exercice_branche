c6657ca4206d20ae0fe125797d11d2e75103f729
Samir <samirelorf@yahoo.fr>
2025-06-13 12:40:50 +0200

    Test

--- syntaxe/debug.js
+++ syntaxe/debug.js
@@ -3,19 +3,18 @@
 // Prix de deux articles
 let a1 = 10
 let a2 = 20
+
 // Nous calculons le total
 let tot= a1 + a2
 
 // Prix d'un troisieme article
 let a3 = 50
+
 // Nous ajoutons a3 au total
 tot2 = tot + a3 
 
 //Nous appliquons 20% de reduction
+let tot3 = tot2 - (tot*0.2)
 
-let tot2 = tot - (tot*0.2)
-
-
-
-console.log("Le prix total avant reduction est de : ", tot)
-console.log("Le prix total après reduction est de : ", tot2)
+console.log("Le prix total avant reduction est de : ", tot2)
+console.log("Le prix total après reduction est de : ", tot3)
