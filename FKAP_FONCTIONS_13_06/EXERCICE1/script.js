c69dfc2ae0586a73c32f88a168b4fcf3b615ec46
Samir <samirelorf@yahoo.fr>
2025-06-14 09:43:01 +0200

    fonctions exercice 1

---
+++ FKAP_FONCTIONS_13_06/EXERCICE1/script.js
@@ -0,0 +1,29 @@
+
+//EXERCICE 1
+//1.
+/*function addition3Number () {
+const somme = 1 + 2 + 3
+console.log (somme)
+}
+addition3Number()*/
+
+
+//2.
+/*function addition3Number () {
+    let num1 = 1
+    let num2 = 2
+    let num3 = 3
+    const somme = num1 + num2 + num3
+    console.log(`le résultat de la somme est ${somme}`)
+}
+addition3Number()*/
+
+
+//3.
+/*function addition3NumbersWithArg(x, y, z) {
+    const somme = x + y + z;
+    console.log("Le résultat de la somme est " + somme);
+}
+addition3NumbersWithArg(1, 2, 3);
+addition3NumbersWithArg(4, 5, 6);
+addition3NumbersWithArg(7, 8, 9);*/
