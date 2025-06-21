756f251ed3bc68055f59b0c43480e42c521625b6
Samir <samirelorf@yahoo.fr>
2025-06-21 13:33:32 +0200

    exos

---
+++ C04_FONCTIONS/exercice 3/script.js
@@ -0,0 +1,12 @@
+sendMessage("Je te rends visite la semaine prochaine", "Marc", "Linda");
+sendMessage("Super on se voit mardi !", "Linda", "Marc")
+
+let toName = "Jean"
+let fromName = "Ada"
+let Message = "Je t'apprends à coder tes premières fonctions"
+    console.log("From : " + fromName + " to : " + toName + " Message : " + Message)
+
+function sendMessage(Message, fromName, toName)
+{   console.log("From : " + fromName + " to : " + toName + " Message : " + Message)
+
+}
