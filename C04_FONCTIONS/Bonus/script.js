871779c05a8ffbdf9ebb8ec8f822b2157dec1c6d
Samir <samirelorf@yahoo.fr>
2025-06-21 13:55:01 +0200

    bonus

---
+++ C04_FONCTIONS/Bonus/script.js
@@ -0,0 +1,4 @@
+function preparerPate(farine, lait, oeufs, sucre) {
+  return (`Pâte prête avec ${farine}g de farine, + ${lait}ml de lait, + ${oeufs} d'oeufs et ${sucre}g de sucre.`)
+}
+console.log(preparerPate(250, 500, 3, 50));
