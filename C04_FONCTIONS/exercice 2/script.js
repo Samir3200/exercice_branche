9f85aa671a521d71c79042ace59b1c2e6a742404
Samir <samirelorf@yahoo.fr>
2025-06-21 12:13:47 +0200

    rectification

---
+++ C04_FONCTIONS/exercice 2/script.js
@@ -0,0 +1,28 @@
+//calcule le carré d'un nombre
+
+function carre(nombre) {  
+    const result =  nombre * nombre
+        console.log (result)
+        return result
+}
+carre(3)
+carre(2)
+
+function multiplierParDix(nombre) {
+    const produit = nombre * 10
+        console.log (produit)
+        return produit
+}
+multiplierParDix(9)
+
+
+
+// JE N'AI PAS REUSSI A FAIRE CET EXERCICE
+/*prompt()
+let saisie = prompt()
+let number = parseInt(saisie)
+let resultatCarre = carre(number)
+  console.log(resultatCarre)
+let resultatFinal = multiplierParDix(resultatCarre)
+  console.log(resultatFinal)*/
+
