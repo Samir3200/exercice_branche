caee08a93e1a54704bdcefb44a56cf97e2fdc36a
Samir <samirelorf@yahoo.fr>
2025-06-21 15:44:03 +0200

    mise à jour

---
+++ C04_FONCTIONS/Bonus exercice 4/scriptetape2.js
@@ -0,0 +1,9 @@
+
+let farine = 250;
+let lait = 500;
+let oeufs = 3;
+let sucre = 50;
+function cuireCrepe(farine, lait, oeufs, sucre) {
+  return (`Crêpe cuite avec ${farine}g de farine, ${lait}ml de lait, ${oeufs} oeufs et ${sucre}g de sucre.`)
+}
+console.log(cuireCrepe(farine, lait, oeufs, sucre));
