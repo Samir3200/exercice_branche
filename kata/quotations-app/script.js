571379061642f8262690d582ca400d53472ae999
Samir <samirelorf@yahoo.fr>
2025-07-04 11:24:48 +0200

    etape 1 à 3

---
+++ kata/quotations-app/script.js
@@ -0,0 +1,10 @@
+
+const container = (citation,author)
+const citation = document.getElementById("text")
+const author = document.getElementById("author")
+
+.addEventListener("input", ) () => {
+    citation.innerText="text"
+
+
+}
