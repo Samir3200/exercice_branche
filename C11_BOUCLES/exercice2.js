c1fe935cc050c4795a178664b8b9830b82d0d5aa
Samir <samirelorf@yahoo.fr>
2025-07-09 16:04:11 +0200

    final

--- C11_BOUCLES/exercice2.js
+++ C11_BOUCLES/exercice2.js
@@ -1,6 +1,6 @@
 let message = ""
-//let letters = [ 'A', 'd', 'a', ' ', 'T', 'e', 'c', 'h']
-let letters = ['A', 'd', 'a', ' ', 'S', 'c', 'h', 'o', 'o', 'l']
+let letters = [ 'A', 'd', 'a', ' ', 'T', 'e', 'c', 'h']
+//let letters = ['A', 'd', 'a', ' ', 'S', 'c', 'h', 'o', 'o', 'l']
 for (let i = 0; i < letters.length; i++) {
     message += letters[i]
 }
